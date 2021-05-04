5547a236f02987e2aa5011231483293bf35b2027
Antenor <antenorzapata@gmail.com>
2021-05-04 19:42:43 -0300

    emailjs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,9 @@ import Song from '../components/Player/Song';
 import Header from '../components/Header';
 import image from '../components/Player/images/noks.jpg';
 import audio from '../components/Player/music/pescoco.mp3';
+import emailjs from 'emailjs-com';
+import { init } from 'emailjs-com';
+init('user_XUCQW0dqHQK2Uh4J45aox');
 
 const audioVoice = [
   {
@@ -34,6 +37,27 @@ export default class About extends Component {
     this.setState({ isPlaying: !isPlaying });
   }
 
+  handleSendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_uhlkyfm',
+        'template_ehpw22u',
+        e.target,
+        'user_XUCQW0dqHQK2Uh4J45aox'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   render() {
     const { currSong, isPlaying } = this.state;
     console.log(isPlaying);
@@ -49,9 +73,21 @@ export default class About extends Component {
               currSong={currSong}
             />
           </div>
+
           <div className="form">
-            <form action="">
-              <input type="text" />
+            <form onSubmit={this.handleSendEmail}>
+              <div className="about-name">
+                <input type="text" placeholder="Nome" required name="name" />
+              </div>
+              <div className="about-email">
+                <input type="email" placeholder="Email" required name="email" />
+              </div>
+              <div className="about-textarea">
+                <textarea id="" cols="20" rows="10" name="message"></textarea>
+              </div>
+              <div className="about-btn">
+                <button type="submit">Enviar</button>
+              </div>
             </form>
           </div>
         </div>
